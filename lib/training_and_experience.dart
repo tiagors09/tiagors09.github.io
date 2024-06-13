64307698a04e9b97f48573c7fc13d22abfd1bca0
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-06-13 11:13:07 -0300

    feat: :sparkles: Add new screens

---
+++ lib/training_and_experience.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TrainingAndExperience extends StatelessWidget {
+  const TrainingAndExperience({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Formação e Experiência'),
+    );
+  }
+}
