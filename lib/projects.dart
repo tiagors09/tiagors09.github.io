e7d9547799bf66ff56e1b752b68dddf9c63a1737
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-11-05 10:17:08 -0300

    update

--- lib/projects.dart
+++ lib/projects.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 class Projects extends StatelessWidget {
@@ -9,61 +10,65 @@ class Projects extends StatelessWidget {
     final projects = [
       {
         'name': 'bmi calculator',
-        'description': 'Uma calculadora de Índice de Massa Corporal (IMC).',
+        'description': 'Calculadora prática de IMC.',
         'site': 'https://bmi-calculator-tiagors09.vercel.app/',
+        'github': 'https://github.com/tiagors09/bmi-calculator',
         'thumb': 'assets/images/bmi_calculator.png',
       },
       {
         'name': 'make your burger',
-        'description':
-            'Crie seu próprio hambúrguer personalizado com este aplicativo interativo.',
+        'description': 'Monte seu hambúrguer ideal.',
         'site': 'https://make-your-burger-six.vercel.app/',
+        'github': 'https://github.com/tiagors09/make-your-burger',
         'thumb': 'assets/images/make_your_burger.png',
       },
       {
         'name': 'expenses',
-        'description':
-            'Um aplicativo para gerenciar suas despesas financeiras de maneira eficiente e fácil.',
+        'description': 'Gerencie suas despesas fácil.',
         'site': 'https://expenses-sooty.vercel.app/',
+        'github': 'https://github.com/tiagors09/expenses',
         'thumb': 'assets/images/expenses.png',
       },
       {
         'name': 'meals',
-        'description': 'Aplicativo intuitivo de receitas.',
+        'description': 'Aplicativo simples de receitas.',
         'site': 'https://meals-eight-zeta.vercel.app/',
+        'github': 'https://github.com/tiagors09/meals',
         'thumb': 'assets/images/meals.png',
       },
       {
         'name': 'shop',
-        'description':
-            'Uma loja virtual com uma variedade de produtos disponíveis para venda online.',
+        'description': 'Loja virtual com produtos.',
         'site': 'https://tiagors09.github.io/shop/',
+        'github': 'https://github.com/tiagors09/shop',
         'thumb': 'assets/images/shop.png',
       },
       {
         'name': 'calculator',
-        'description': 'Calculadora simples.',
+        'description': 'Calculadora simples e útil.',
         'site': 'https://calculator-tau-one-62.vercel.app/',
+        'github': 'https://github.com/tiagors09/calculator',
         'thumb': 'assets/images/calculator.png',
       },
       {
         'name': 'great places',
-        'description': 'App para guardar locais favoritos.',
-        'site': 'https://github.com/tiagors09/great_places',
+        'description': 'Guarde seus locais favoritos.',
+        'site': '',
+        'github': 'https://github.com/tiagors09/great_places',
         'thumb': 'assets/images/maps.jpg',
       },
       {
         'name': 'OlhoVivoSP',
-        'description':
-            'App para consulta de corredores, paradas e previsão de chegada de ônibus de São Paulo.',
-        'site': 'https://github.com/tiagors09/teste-android-estagio-v1',
+        'description': 'Consulte ônibus de São Paulo.',
+        'site': '',
+        'github': 'https://github.com/tiagors09/teste-android-estagio-v1',
         'thumb': 'assets/images/sao_paulo.jpg',
       },
       {
         'name': 'Calorie Way',
-        'description':
-            'Um aplicativo para ajudar os usuários a calcular a ingestão diária recomendada de calorias com base no seu perfil físico e objetivos de peso.',
-        'site': 'https://github.com/tiagors09/calorie_way',
+        'description': 'Calcule suas calorias diárias.',
+        'github': 'https://github.com/tiagors09/calorie_way',
+        'site': 'https://calorie-way.vercel.app/',
         'thumb': 'assets/images/calorie_way.png',
       }
     ];
@@ -72,86 +77,81 @@ class Projects extends StatelessWidget {
         MediaQuery.of(context).size.width <= 480;
 
     return GridView.builder(
-      padding: const EdgeInsets.only(
-        top: 20,
-        left: 20,
-        right: 20,
-      ),
       itemCount: projects.length,
       itemBuilder: (ctx, i) {
-        return InkWell(
-          onTap: () {
-            canLaunchUrlString(projects[i]['site']!).then(
-              (value) {
-                if (value) {
-                  launchUrlString(projects[i]['site']!);
-                }
-              },
-            );
-          },
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15),
-            ),
-            elevation: 4,
-            margin: const EdgeInsets.all(10),
-            child: Stack(
-              children: <Widget>[
-                ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(15),
-                    topRight: Radius.circular(15),
-                    bottomLeft: Radius.circular(15),
-                    bottomRight: Radius.circular(15),
-                  ),
-                  child: Image.asset(
-                    projects[i]['thumb']!,
-                    height: 250,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                    errorBuilder: (context, error, stackTrace) =>
-                        const Text('Failed to load image...'),
-                  ),
-                ),
-                Positioned(
-                  left: 20,
-                  right: 20,
-                  bottom: 10,
-                  child: Container(
-                    width: isSmartphone ? 250 : 300,
-                    color: Colors.black87,
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 5,
-                      horizontal: 20,
+        return Container(
+          margin: const EdgeInsets.all(8),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: GridTile(
+              footer: GridTileBar(
+                backgroundColor: Colors.black87,
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      projects[i]['name']!,
+                      style: TextStyle(
+                        fontSize: isSmartphone ? 15 : 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
                     ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          projects[i]['name']!,
-                          style: TextStyle(
-                            fontSize: isSmartphone ? 15 : 20,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600,
-                          ),
-                          softWrap: true,
-                          overflow: TextOverflow.fade,
-                        ),
-                        Text(
-                          projects[i]['description']!,
-                          textAlign: TextAlign.justify,
-                          style: TextStyle(
-                            fontSize: isSmartphone ? 13 : 15,
-                            color: Colors.white,
-                          ),
-                          softWrap: true,
-                          overflow: TextOverflow.fade,
-                        ),
-                      ],
+                    Text(
+                      projects[i]['description']!,
+                      style: TextStyle(
+                        fontSize: isSmartphone ? 13 : 15,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
                     ),
-                  ),
+                  ],
                 ),
-              ],
+                trailing: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        canLaunchUrlString(projects[i]['site']!).then(
+                          (value) {
+                            if (value) {
+                              launchUrlString(projects[i]['site']!);
+                            }
+                          },
+                        );
+                      },
+                      icon: const Icon(
+                        FontAwesomeIcons.link,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        canLaunchUrlString(projects[i]['github']!).then(
+                          (value) {
+                            if (value) {
+                              launchUrlString(projects[i]['github']!);
+                            }
+                          },
+                        );
+                      },
+                      icon: const Icon(
+                        FontAwesomeIcons.github,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              child: Image.asset(
+                projects[i]['thumb']!,
+                height: 250,
+                width: double.infinity,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) =>
+                    const Text('Failed to load image...'),
+              ),
             ),
           ),
         );
